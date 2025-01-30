2a3898904c6edcdce87b23e686236612cc9d813c
Mohammad Kashif Khan <kk11122002@gmail.com>
2025-01-30 15:53:43 +0530

    commit 2

--- Graph_utils.py
+++ Graph_utils.py
@@ -1,5 +1,5 @@
 import random
-
+import matplotlib.pyplot as plt
 import networkx as nx
 
 def random_graph(peers):
@@ -31,5 +31,41 @@ def random_graph(peers):
         print()
 
     return P2P_network
+
 def isConnected(G : nx.Graph):
     return nx.is_connected(G)
+
+def P2P_network_generate(n_peers):
+
+    peers = [Peer(i + 1) for i in range(n_peers)]
+
+    slow = z0 * n_peers
+    lowcpu = z1 * n_peers
+
+    slow_peers = random.sample(peers, int(slow))
+    lowcpu_peers = random.sample(peers, int(lowcpu))
+
+    for peer in slow_peers:
+        peer.isSlow = True
+
+    for peer in lowcpu_peers:
+        peer.isLowCPU = True
+
+    P2P_network = random_graph(peers)
+
+    min_degree = min([degree for _ , degree in P2P_network.degree()])
+    print("Min degree", min_degree)
+    max_degree = max([degree for _ , degree in P2P_network.degree()])
+    print("Max degree", max_degree)
+    while (not isConnected(P2P_network)) or min_degree < 3:
+        P2P_network = random_graph(peers)
+        min_degree = min([degree for _, degree in P2P_network.degree()])
+        print("Min degree", min_degree)
+        max_degree = max([degree for _ , degree in P2P_network.degree()])
+        print("Max degree", max_degree)
+
+    nx.draw(P2P_network, with_labels=True)
+    plt.savefig('P2P_network.png')
+    plt.clf()
+
+    return P2P_network,peers
--- Peer.py
+++ Peer.py
@@ -13,14 +13,7 @@ class Peer(object):
         self.neighbours = set()
         self.mempool = []
         self.ledger = nx.Graph()
-        self.process = env.process(self.generate_transactions())
 
-    def generate_transactions(self):
-        while True:
-            interarrival_time = random.exponential(1 / self.tx_rate)  # Exponential delay
-            yield self.env.timeout(interarrival_time)  # Wait for the next transaction
-            self.tx_count += 1
-            print(f'Time {self.env.now:.2f}: Node {self.node_id} generated Transaction {self.tx_count}')
 
     def __str__(self):
         return f'Peer Id {self.ID} neighbours : {self.neighbours} isslow : {self.isSlow} isHighCPu : {self.isLowCPU} balance : {self.balance}'
